2a4d96930e55fbd059184f5d3efb1c77dc2a15dc
Doohwancho <doohwancho1993@gmail.com>
2023-12-08 17:23:07 +0900

    front-authentication-style-6: styled applied on login page

--- front/ecommerce/src/pages/Login.tsx
+++ front/ecommerce/src/pages/Login.tsx
@@ -1,6 +1,80 @@
 import React, { useState } from 'react';
 import { useMutation, UseMutationResult } from 'react-query';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+// Reuse the styled components from the Register component
+const FormContainer = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: #f7f7f7;
+`;
+
+const FormTitle = styled.h2`
+  font-size: 2rem;
+  color: #333;
+  // margin-bottom: 20px;
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+`;
+
+const Form = styled.form`
+  display: grid;
+  grid-gap: 15px;
+  width: 100%;
+  max-width: 400px;
+  padding: 20px;
+  border-radius: 8px;
+  background: white;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
+`;
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.label`
+  font-size: 0.9rem;
+  color: #555;
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  border: 2px solid #ddd;
+  border-radius: 4px;
+  font-size: 1rem;
+  &:focus {
+    border-color: #a4c9f3;
+    outline: none;
+  }
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  background-color: #0056b3;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: #003d82;
+  }
+`;
+
+const ErrorMessage = styled.p`
+  color: red;
+`;
+
+// const LoadingMessage = styled.p`
+//   color: #0275d8; // Bootstrap's info color
+//   font-size: 1rem;
+//   text-align: center;
+// `;
 
 interface LoginRequestDTO {
     'username': string;
@@ -9,7 +83,7 @@ interface LoginRequestDTO {
 
 // Assuming a simple login with username and password
 const initialLoginData = {
-    username: '',
+    username: 'test-id',
     password: '',
   };  
 
@@ -66,34 +140,22 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={loginData.username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={loginData.password}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      {isLoading && <p>Logging in...</p>}
-      {isError && <p>Error: {error.message}</p>}
-    </div>
+    <FormContainer>
+      <Form onSubmit={handleSubmit}>
+        <FormTitle>Login</FormTitle>
+        <FormGroup>
+          <Label htmlFor="username">Username:</Label>
+          <Input type="text" id="username" name="username" value={loginData.username} onChange={handleChange} />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="password">Password:</Label>
+          <Input type="password" id="password" name="password" value={loginData.password} onChange={handleChange} />
+        </FormGroup>
+        <Button type="submit">Login</Button>
+      </Form>
+      {/* {isLoading && <LoadingMessage>Logging in...</LoadingMessage>} */}
+      {isError && <ErrorMessage>Error: {error.message}</ErrorMessage>}
+    </FormContainer>
   );
 };
 
