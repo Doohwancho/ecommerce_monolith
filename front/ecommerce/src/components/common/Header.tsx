eaf8618c323d658833dab5661ed2b8ca7a70e87e
Doohwancho <doohwancho1993@gmail.com>
2023-12-10 16:31:11 +0900

    front-authentication-style-8: style for header component

---
+++ front/ecommerce/src/components/common/Header.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const TopWrapper = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #f2f2f2;
+  padding: 0.5vh 3vw;
+
+  @media screen and (max-width: 640px) {
+    display: none;
+  }
+`;
+
+const TopRight = styled.ul`
+  display: flex;
+
+  li {
+    padding-left: 1vw;
+    font-size: 0.8rem;
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
+`;
+
+const Header: React.FC = () => {
+
+  const logout = () => {
+    axios
+      .post("http://127.0.0.1:8080/logout", { credentials: 'include' })
+      .then(() => {
+        alert('로그 아웃 성공!');
+      })
+      .catch(() => {
+        alert('로그 아웃 실패!');
+      });
+  };
+
+  return (
+    <>
+       <TopWrapper>
+         <div></div>
+         <TopRight>
+           <Link to="/register">
+             <li>회원가입</li>
+           </Link>
+           <li onClick= {logout}>로그아웃</li>
+           <Link to="/login">
+             <li>로그인</li>
+           </Link>
+         </TopRight>
+       </TopWrapper>
+    </>
+  );
+};
+
+export default Header;
