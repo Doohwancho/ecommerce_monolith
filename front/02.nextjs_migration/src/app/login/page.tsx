43d1f32de9af1d154dad9a38f523717d68e776cf
Doohwancho <doohwancho1993@gmail.com>
2024-08-22 04:10:27 +0900

    front-authentication-feat-16: make register, login page SSG rendering

--- front/02.nextjs_migration/src/app/login/page.tsx
+++ front/02.nextjs_migration/src/app/login/page.tsx
@@ -2,6 +2,8 @@ import Footer from "@/components/organism/footer";
 import { LoginForm } from "@/components/organism/loginForm";
 import HeaderMainFooterLayout from "@/components/template/headerMainFooterLayout";
 
+export const dynamic = "force-static";
+
 export default function LoginPage() {
   return (
     <>
--- front/02.nextjs_migration/src/app/register/page.tsx
+++ front/02.nextjs_migration/src/app/register/page.tsx
@@ -2,6 +2,8 @@ import Footer from "@/components/organism/footer";
 import { RegisterForm } from "../../components/organism/registerForm";
 import HeaderMainFooterLayout from "@/components/template/headerMainFooterLayout";
 
+export const dynamic = 'force-static' 
+
 export default function RegisterPage() {
   return (
     <>
