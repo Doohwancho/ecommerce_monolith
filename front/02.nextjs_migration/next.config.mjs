36957eea99916fcd93c771428ca37b5576396dcf
Doohwancho <doohwancho1993@gmail.com>
2024-11-13 16:35:58 +0900

    front-build-6: nextjs에서 개발할 때 cache 때문에 값이 바로 안되는 문제를 production mode 아니면 캐시 끄도록 설정

--- front/02.nextjs_migration/next.config.mjs
+++ front/02.nextjs_migration/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isDev = process.env.NODE_ENV !== 'production';
+
+const nextConfig = {
+  async headers() {
+    return isDev
+      ? [
+          {
+            source: '/(.*)',
+            headers: [
+              { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, proxy-revalidate' },
+              { key: 'Pragma', value: 'no-cache' },
+              { key: 'Expires', value: '0' },
+            ],
+          },
+        ]
+      : [];
+  },
+};
 
 export default nextConfig;
